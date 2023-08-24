710b50148e51bd0a8828cd8d4b36e4bbb25de427
Kelly Nielsen (lap) <kelly.nielsen@vivint.com>
2023-08-24 16:37:33 -0600

    Remove remnants of Swagger

--- XorTag/Program.cs
+++ XorTag/Program.cs
@@ -36,15 +36,10 @@ namespace XorTag
 
             builder.Services.AddControllers();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            //builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            //app.UseSwagger();
-            //app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseMiddleware<ExceptionMiddleware>();
